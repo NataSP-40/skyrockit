0c8e06cbb7d74d90e6dec58a57b7dcf0d5f51fb3
NataSP-40 <natalia.spricop@gmail.com>
2025-09-03 14:46:31 -0500

    new  files

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+//require our new middlware
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
+const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
-// Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3000";
+
+const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
+mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-// Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
-// Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"));
-// Morgan for logging HTTP requests
-app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passUserToView); // use new passUserToView middleware here
+
+app.get('/', (req, res) => { // welcome page comes before isSignedIn middleware
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
+});
+
+app.use('/auth', authController);
+
+app.use(isSignedIn); // use new isSignedIn middleware here
+// IMPORTANCE of arrangements----- it can/t be before index.ejs, a show page must appear first... 
+// this will not run before the other routes
+app.use('/users/:userId/applications', applicationsController); 
+
+
+//------ROUTES here
+
+
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
